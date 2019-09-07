161c05f0fa87455609613af8a2a0c6a48db1febd
duicul <duicul@yahoo.com>
2019-09-07 16:38:47 +0300

    Training process modifications

--- dataoperations.py
+++ dataoperations.py
@@ -2,7 +2,10 @@ import numpy as np
 import random
 
 def output_condition(inp):
-    return (( inp[1]>=8/24 and inp[1]<19/24 and inp[0]>=1/7 and inp[0]<=5/7 ) or (inp[1]>=10/24 and inp[1]<14/24 and inp[0]>=6/7 and inp[0]<=7/7))
+    cond1=inp[1]>=8/24 and inp[1]<19/24 and inp[0]>=1/7 and inp[0]<=5/7 
+    cond2=inp[1]>=10/24 and inp[1]<14/24 and inp[0]>=6/7 and inp[0]<=7/7
+    cond3=inp[1]>=20/24 and inp[1]<24/24 and inp[0]>=3/7 and inp[0]<=5/7
+    return (cond1 or cond2 or cond3 )
 
 def generate_inputs(number):
     return np.array([[random.randint(1,7)/7,random.randint(1,24)/24,random.randint(1,60)/60]for i in range(number)])
@@ -21,5 +24,5 @@ def generate_data(size):
             #y_val=1 if inp[0]>inp[1] and inp[0] < 1-inp[1] else 0
             #print("{} {}".format(inp,y_val))
         y_train.append(y_val)
-    print("Count on {} | Count off {} -> {}%".format(count_on,len(x_train)-count_on,count_on/len(x_train)*100))
-    return [x_train,y_train]
+    #print("Count on {} | Count off {} -> {}%".format(count_on,len(x_train)-count_on,count_on/len(x_train)*100))
+    return {"count_on":count_on,"count_off":len(x_train)-count_on,"percent_on":count_on/len(x_train)*100,"data":[x_train,y_train]}
--- test_train.py
+++ test_train.py
@@ -8,80 +8,150 @@ from keras.models import load_model
 import time
 import numpy as np
 from dataoperations import output_condition,generate_inputs,generate_data
+import matplotlib.pyplot as plt
 import json
 
 #x_train = np.random.rand(10, 2)
 
-model = Sequential()
-model.add(Dense(10, activation='sigmoid', input_dim=3))
-#model.add(Dropout(0.5))
-#model.add(Dense(30, activation='linear'))
-#model.add(Dense(10, activation='linear'))
-model.add(Dense(6, activation='sigmoid'))
-model.add(Dense(3, activation='sigmoid'))
-#model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+def train(training_data_size,model_file,outfile,mode):
+    print("Training model ")
+    f = open(outfile, mode)
+    outstring=""
+    model = Sequential()
+    model.add(Dense(10, activation='sigmoid', input_dim=3))
+    #model.add(Dropout(0.5))
+    #model.add(Dense(30, activation='linear'))
+    #model.add(Dense(10, activation='linear'))
+    model.add(Dense(6, activation='sigmoid'))
+    model.add(Dense(3, activation='sigmoid'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-sgd = SGD(lr=0.5, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='mean_squared_error',
-              optimizer=sgd,
-              metrics=['accuracy'])
-total_time=time.process_time_ns()
-data_total=[]
-loss=1
-acc=0
-count_loops=0
-plateau_loops=0
-loss=1
-acc=0
-dloss=0
-while (loss > 0.005 or acc < 0.995):
-    print("Training batch {}".format(count_loops))
-    data=generate_data(1000)
-    time_count=time.process_time_ns()
-    model.fit(data[0], data[1],
-          epochs=80,verbose=0,shuffle=True)#batch_size=50
-    time_count=time.process_time_ns()-time_count
-    loss_metr=model.evaluate(data[0], data[1],verbose=0)
-    print("dloss={} 1%loss={}".format(dloss,loss_metr[0]/100))
-    if dloss < 0  or dloss<(loss_metr[0]-loss)/2:
-        plateau_loops=plateau_loops+1
-    else : plateau_loops=0
-    dloss=loss_metr[0]-loss
-    if plateau_loops >= 3 :
-        plateau_loops=0
+    sgd = SGD(lr=0.5, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='mean_squared_error',optimizer=sgd,metrics=['accuracy'])
+    total_time=time.process_time_ns()
+    data_total=[]
+    loss=1
+    acc=0
+    count_loops=0
+    plateau_loops=0
+    loss=1
+    acc=0
+    dloss=0
+    epochs=[]
+    lrs=[]
+    errors=[]
+    accuracys=[]
+    plateau_loopss=[]
+    while (loss > 0.005 or acc < 0.995):
+        outstring+="Training batch {} \n".format(count_loops)
+        data=generate_data(training_data_size)
+        outstring+="Count on {} | Count off {}  Percent on {} % \n".format(data["count_on"],data["count_off"],data["percent_on"])
+        time_count=time.process_time_ns()
+        model.fit(data['data'][0], data['data'][1],epochs=80,verbose=0,shuffle=True)#batch_size=50
+        time_count=time.process_time_ns()-time_count
+        loss_metr=model.evaluate(data['data'][0], data['data'][1],verbose=0)
+        outstring+="dloss={} 1%loss={} \n".format(dloss,loss_metr[0]/100)
+        if dloss < 0  or dloss<(loss_metr[0]-loss)/2:
+            plateau_loops=plateau_loops+1
+        else :
+            plateau_loops=0
+            K.set_value(model.optimizer.lr,0.5)  
+        dloss=loss_metr[0]-loss
+        loss=loss_metr[0]
+        acc=loss_metr[1]
+        epochs.append(count_loops)
+        lrs.append(K.get_value(model.optimizer.lr))
+        errors.append(loss_metr[0])
+        accuracys.append(loss_metr[1])
+        plateau_loopss.append(plateau_loops)
+        count_loops=count_loops+1
+        if plateau_loops >= 3 :
+            plateau_loops=0
         K.set_value(model.optimizer.lr,K.get_value(model.optimizer.lr)*3/4)
-    loss=loss_metr[0]
-    acc=loss_metr[1]
-    count_loops=count_loops+1
-    print("{} Loss + metrics {} , lr = {}".format(plateau_loops,loss_metr,K.get_value(model.optimizer.lr)))
-    print(model.metrics_names)
-    print("Training time {} = {}s".format(time_count,time_count/1000000000))
-    #add to data_total
-    for i in range(len(data[0])):
-        data_total.append({"inp":list(data[0][i]),"outp":data[1][i]})
-with open('testvalues.json', 'w') as outfile:
-    json.dump(data_total, outfile)
-total_time=time.process_time_ns()-total_time
-print("Toatal training time {} = {}s for {} training sets ".format(total_time,total_time/1000000000,count_loops))
-model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+        outstring+="{} Loss + metrics {} , lr = {}\n".format(plateau_loops,loss_metr,K.get_value(model.optimizer.lr))
+        outstring+=str(model.metrics_names)+"\n"
+        outstring+="Training time {} = {}s \n".format(time_count,time_count/1000000000)
+        #add to data_total
+        for i in range(len(data['data'][0])):
+            data_total.append({"inp":list(data['data'][0][i]),"outp":data['data'][1][i]})
+    with open(outfile+'.json', 'a') as outfile:
+        json.dump(data_total, outfile)
+    total_time=time.process_time_ns()-total_time
+    outstring+="Toatal training time {} = {}s for {} training sets \n".format(total_time,total_time/1000000000,count_loops)
+    model.save(model_file)  # creates a HDF5 file 'my_model.h5'
+    f.write(outstring)
+    f.close()
+    print("Finished training model ")
+    return {"epochs":epochs,"error":errors,"lr":lrs,"acc":accuracys,"plateau":plateau_loopss}
 
-#model = load_model('my_model.h5')
-#print(x_train)
-#print(y_train)
+def test_model(data_size,model_name,outfile,mode):
+    print("Testing model ")
+    f = open(outfile, mode)
+    outstring=""
+    model = load_model(model_name)
+    data_test=generate_data(data_size)
+    outstring+="Count on {} | Count off {}  Percent on {} % \n".format(data_test["count_on"],data_test["count_off"],data_test["percent_on"])
+    time_count=time.process_time_ns()
+    t=model.predict(data_test["data"][0],verbose=0)
+    time_count=time.process_time_ns()-time_count
+    outstring+="Predict time {} = {}s\n".format(time_count,time_count/1000000000)
+    count_miss=0
+    missed=[]
+    for i in range(len(t)):
+        pred_val=0 if t[i][0]<0.5 else 1
+        #print("{} {}".format(x_test[i],pred_val))
+        if pred_val != data_test["data"][1][i]:
+            count_miss= (count_miss + 1)
+            missed.append([data_test["data"][0][i],pred_val])
+            outstring+="Wrong {} true_val {} != {} predict\n".format(data_test["data"][0][i],data_test["data"][1][i],t[i])
+    outstring+="Missed {} in {} = {}% \n".format(count_miss,len(t),count_miss/len(t)*100)
+    f.write(outstring)
+    f.close()
+    print("Finished testing model ")
 
-data_test=generate_data(1000)
-time_count=time.process_time_ns()
-t=model.predict(data_test[0],verbose=0)
-time_count=time.process_time_ns()-time_count
-print("Predict time {} = {}s".format(time_count,time_count/1000000000))
-count_miss=0
-missed=[]
-for i in range(len(t)):
-    pred_val=0 if t[i][0]<0.5 else 1
-    #print("{} {}".format(x_test[i],pred_val))
-    if pred_val != data_test[1][i]:
-        count_miss= (count_miss + 1)
-        missed.append([data_test[0][i],pred_val])
-        print("Wrong {} true_val {} != {} predict".format(data_test[0][i],data_test[1][i],t[i]))
-print("Missed {} in {} = {}%".format(count_miss,len(t),count_miss/len(t)*100))
+def plotmodel(out,plot_file):
+    fig, ax = plt.subplots(2,1)
+    print(ax)
+    print(fig)
+    ax1=ax[0]
+    ax2=ax[1]
+    ax1.grid(True)
+    ax2.grid(True)
+    #ax1=plt.subplot(221)
+    ax1.plot(out['epochs'],out['lr'],label="Learning Rate")
+    #ax1.legend(loc='upper left')
+    #ax1.xlabel('Epochs')
+    #ax1.ylabel('Learning Rate')
+
+    #ax4=plt.subplot(224)
+    ax1.plot(out['epochs'],out['plateau'],label="Plateau")
+    #ax4.legend(loc='upper left')
+    #fig.xlabel('Epochs')
+    #ax1.ylabel('Plateau')
+    ax1.legend()
+    
+    #ax2=plt.subplot(222)
+    ax2.plot(out['epochs'],out['error'],label="Error")
+    #ax2.legend(loc='upper left')
+    #ax2.xlabel('Epochs')
+    #ax2.ylabel('Error')
+
+    #ax3=plt.subplot(223)
+    ax2.plot(out['epochs'],out['acc'],label="Accuracy")
+    #ax3.legend(loc='upper left')
+    #fig.xlabel('Epochs')
+    #ax2.ylabel('Accuracy')
+
+    ax2.legend()
+    
+    
+    fig.savefig(plot_file)
+
+out=train(1000,"my_model1.h5","model1train.txt","wt")
+test_model(1000,"my_model1.h5","model1test.txt","wt")
+plotmodel(out,"1000samples.png")
+
+out=train(10000,"my_model2.h5","model2train.txt","wt")
+test_model(10000,"my_model2.h5","model2test.txt","wt")
+plotmodel(out,"10000samples.png")
